e3bf74e8e34e5057a3fa47f4f0301729a5a38bd7
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-04-16 18:24:57 -0400

    Update sketch.js
    rainfall with my bolt. Not working

--- sketch.js
+++ sketch.js
@@ -1,11 +1,43 @@
+var electric = [];
+
 function setup() {
   createCanvas(400, 400);
+  for (var i = 0; i <3; i++){
+      electric[i] = new zip();
+  }
+
 }
 
 function draw() {
   background(220);
   
-  bolt();
+  for (var i = 0; i < 3; i++) {
+    electric[i].show();
+    electric[i].update();
+  }
+
+  //bolt();
   
-  text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
+  //text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
+}
+
+function zip(){
+  this.x = random(0,width);
+  this.y = random (0, -height);
+  
+  this.show = function(){
+    bolt();
+  }
+  
+  this.update = function (){
+    this.speed = random(5,10);
+    this.gravity = 1.05;
+    this.y = this.y + this.speed*this.gravity;
+    
+    if (this.y > height){
+      this.y = random (0, -height);
+      this.gravity = 0
+    }
+  }
+  
 }
