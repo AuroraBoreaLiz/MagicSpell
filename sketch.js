eec465bc70e714d7366ec31988c9fca7c53515fb
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-04-16 18:06:59 -0400

    Update sketch.js
    moving bolt out to its own function

--- sketch.js
+++ sketch.js
@@ -4,18 +4,8 @@ function setup() {
 
 function draw() {
   background(220);
-  n = noise();
-  noStroke();
-  beginShape();
-    vertex(313, 137);
-    vertex(311, 177);
-    vertex(261, 210);
-    vertex(260, 232);
-    vertex(231, 263);
-    vertex(252, 196);
-    vertex(282, 180);
-    vertex(305, 149);
-  endShape(CLOSE);
+  
+  bolt();
   
   text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
 }
