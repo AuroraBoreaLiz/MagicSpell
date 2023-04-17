7bf8102e56ab9134c8c571a0f55a1903e3771daa
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-04-16 21:04:04 -0400

    Update sketch.js
    ridiculous rain bolts

--- sketch.js
+++ sketch.js
@@ -24,29 +24,10 @@ function draw() {
 function zip(){
   this.x = random(0,width);
   this.y = random (0, -height);
-  this.n = random(230,250)
-  this.n2 = random(230,250);
-  this.n3 = random(230,250);
-  this.n4 = random(250,280);
-  this.n6 = random(250,260);
   
   this.show = function(){
-    //bolt();
-    ellipse(this.n,this.y,this.n6);
-    /*
-    translate(-200,-200);
-    noStroke();
-    beginShape();
-      vertex(this.n+70, 137);
-      vertex(this.n+50, 177);
-      vertex(this.n2, 210);
-      vertex(this.n+10, 232);
-      vertex(this.n-15, 263);
-      vertex(this.n2, 196);
-      vertex(this.n4, 180);
-      vertex(this.n3+40, 149);
-    endShape(CLOSE);
-    */
+    translate(this.x, this.y);
+    bolt();
   }
   
   this.update = function (){
